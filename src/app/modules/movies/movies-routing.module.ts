97ff4354b09af6488880e18fc663c35f58940838
Amaan patel <amaanpatel56@gmail.com>
2022-06-05 00:50:33 +0530

    wip

--- src/app/modules/movies/movies-routing.module.ts
+++ src/app/modules/movies/movies-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {AddMoviesComponent} from './components/add-movies/add-movies.component'
 import { ListMoviesComponent } from './components/list-movies/list-movies.component';
+import { AboutProjectComponent } from './components/about-project/about-project.component';
 
 const routes: Routes = [
   {
@@ -16,7 +17,7 @@ const routes: Routes = [
   },
   {
     path: 'aboutproject',
-    component: AddMoviesComponent,
+    component: AboutProjectComponent,
     data: { title: 'about project' }
   }
 ];
